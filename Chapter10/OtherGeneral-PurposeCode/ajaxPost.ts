ecf376ca385f0a1fc6fbc07eeee9c81a695d6818
nisshimo <shozemi.nishimoto@gmail.com>
2023-06-09 17:08:50 +0900

    update formatPretty

--- Chapter10/OtherGeneral-PurposeCode/ajaxPost.ts
+++ Chapter10/OtherGeneral-PurposeCode/ajaxPost.ts
@@ -1,10 +1,15 @@
-const formatPretty = (obj) => {
-  var str = "{\n";
-  for (var key in obj) {
-      str += "  " + key + " = " + obj[key] + "\n";
-  }
-  return str + "}";
-}
+const formatPretty = (obj, indent = "") => {
+  // Handle null, undefined, strings, and non-objects. if (obj === null) return "null";
+  if (obj === undefined) return "undefined";
+  if (typeof obj === "string") return '"' + obj + '"'; if (typeof obj !== "object") return String(obj);
+          if (indent === undefined) indent = "";
+          // Handle (non-null) objects.
+          var str = "{\n";
+          for (var key in obj) {
+              str += indent + "  " + key + " = ";
+  str += formatPretty(obj[key], indent + " ") + "\n"; }
+          return str + indent + "}";
+      };
 
 export const ajaxPost = async ({
   url,
