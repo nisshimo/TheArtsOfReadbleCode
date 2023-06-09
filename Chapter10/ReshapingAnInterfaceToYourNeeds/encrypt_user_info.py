01ecd42c028c2e41c793e6e893af64a422cdbada
nisshimo <shozemi.nishimoto@gmail.com>
2023-06-09 20:59:53 +0900

    bad example

--- Chapter10/ReshapingAnInterfaceToYourNeeds/encrypt_user_info.py
+++ Chapter10/ReshapingAnInterfaceToYourNeeds/encrypt_user_info.py
@@ -6,13 +6,26 @@ PRIVATE_KEY = ""
 INIT_VECTOR= []
 ENCODE = ""
 
-def url_safe_encrypt(obj):
-    obj_str = json.dump(obj)
-    cipher = _Cipher("aes_128_cbc", kry=PRIVATE_KEY, init_vector=INIT_VECTOR, op=ENCODE)
-    encrypted_bytes = cipher.update(obj_str)
-    encrypted_bytes += cipher.final() # 現在128ビットのビットブロックをフラッシュする
-    return base64.urlsafe64_encode(encrypted_bytes)
 
+def url_safe_encrypt_obj(obj):
+    obj_str = json.dumps(obj)
+    return url_safe_encrypt_str(obj_str)
+
+
+def url_safe_encrypt_str(data):
+    encrypted_bytes = encrypt(data)
+    return base64.urlsafe_b64encode(encrypted_bytes)
+
+
+def encrypt(data):
+    cipher = make_cipher()
+    encrypted_bytes = cipher.update(data)
+    # flush out any remaining bytes return encrypted_bytes
+    encrypted_bytes += cipher.final()
+
+
+def make_cipher():
+    return _Cipher("aes_128_cbc", key=PRIVATE_KEY, init_vector=INIT_VECTOR, op=ENCODE)
 
 user_info = {"username" : "...", "password": "..."}
 url = "http://example.com/?user_info=" + url_safe_encrypt(user_info)
