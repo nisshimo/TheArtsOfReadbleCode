87e5f7c614e91978fb2bb71f1145c5480c5a5108
nisshimo <shozemi.nishimoto@gmail.com>
2023-06-09 20:54:28 +0900

    refactor

--- Chapter10/ReshapingAnInterfaceToYourNeeds/encrypt_user_info.py
+++ Chapter10/ReshapingAnInterfaceToYourNeeds/encrypt_user_info.py
@@ -6,15 +6,13 @@ PRIVATE_KEY = ""
 INIT_VECTOR= []
 ENCODE = ""
 
-user_info = {"username": "...", "password": "..."}
-user_str = json.dumps(user_info)
-cipher = _Cipher(
-    "aes_128_cbc",
-    key=PRIVATE_KEY,
-    init_vector=INIT_VECTOR,
-    op=ENCODE
-)
-encrypted_bytes = cipher.update(user_str)
-encrypted_bytes += cipher.final()  # flush out the current 128 bit block
-url = "http://example.com/?user_info=" + \
-    base64.urlsafe_b64encode(encrypted_bytes)
+def url_safe_encrypt(obj):
+    obj_str = json.dump(obj)
+    cipher = _Cipher("aes_128_cbc", kry=PRIVATE_KEY, init_vector=INIT_VECTOR, op=ENCODE)
+    encrypted_bytes = cipher.update(obj_str)
+    encrypted_bytes += cipher.final() # 現在128ビットのビットブロックをフラッシュする
+    return base64.urlsafe64_encode(encrypted_bytes)
+
+
+user_info = {"username" : "...", "password": "..."}
+url = "http://example.com/?user_info=" + url_safe_encrypt(user_info)
